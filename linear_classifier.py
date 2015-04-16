0a92d07bc2e65338861dd707b386e1de2b8e95e3
Tobias Norlund <norlundt@stud22-h56.inf.ethz.ch>
2015-04-16 17:36:18 +0200

    Tested PCA followed by LinearRegression. No improvment

---
+++ linear_classifier.py
@@ -0,0 +1,26 @@
+
+import classification_base
+
+(X, Y, X_val, X_test) = classification_base.load(5000, load_val=True)
+
+# -----
+
+import sklearn.linear_model as sklin
+from sklearn import cross_validation
+import numpy as np
+
+classifier = sklin.LogisticRegression()
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, Y, test_size=0.2)
+
+classifier.fit(X_train, y_train)
+
+print("Score: " + str(classifier.score(X_test, y_test)))
+
+# Predict validation set
+Y_val = classifier.predict(X_val)
+#Y_val2 = best2.predict(X_val)
+np.savetxt('result_validation.txt', Y_val, fmt='%i')
+
+print("Done")
+
